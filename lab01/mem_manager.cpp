5db8ee5f399c4c4703d9d98abaf25160b8820a39
Cameron Wilcox <cjwilcox15@gmail.com>
2022-09-12 11:34:37 -0600

    lab01: Started repl mode

--- lab01/mem_manager.cpp
+++ lab01/mem_manager.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <array>
 #include <tuple>
 #include <queue>
+
+const std::string WHITESPACE = " \n\r\t\f\v";
 
 namespace MemManager
 {
@@ -127,31 +130,93 @@ std::array<std::tuple<int, int>, 16> mMemory = {};                      //Track 
 };
 
 
+std::string ltrim(const std::string &s)
+{
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+ 
+std::string rtrim(const std::string &s)
+{
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
 
+std::string trim(const std::string &s) {
+	return rtrim(ltrim(s));
+}
 
-void repl()
+void handle_insert(MemManager::MemorySystem &system, MemManager::Job &job)
 {
 
 }
 
+void print_options()
+{
+    std::cout << "Type 'print' to print system memory | To enter a job type <ID> <Memory_Size> | To quit type 'quit'\n";
+    std::cout << ">>";
+}
+
+void repl()
+{
+    MemManager::MemorySystem system;
+    bool running = true;
+    std::string input;
+    while (running)
+    {
+        print_options();
+        getline(std::cin, input);
+        input = trim(input);
+        if(input == "quit")
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else if(input == "print")
+        {
+            system.print_memory();
+        }
+        else
+        {
+            try
+            {
+                int split = input.find(" ");
+                int ID = std::stoi(input.substr(0,split));
+                int size = std::stoi(input.substr(split));
+                MemManager::Job temp(size, ID);
+                handle_insert(system, temp);
+                
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+        }
+        
+        
+
+    }
+    
+}
+
 int main()
 {
-    MemManager::Job job1(8000, 1);
-    MemManager::Job job2(8000, 2);
-    MemManager::Job job3(65535, 3);
+    repl();
+    // MemManager::Job job1(8000, 1);
+    // MemManager::Job job2(8000, 2);
+    // MemManager::Job job3(65535, 3);
 
-    MemManager::MemorySystem system;
+    // MemManager::MemorySystem system;
 
-    system.add_job(job1);
-    system.add_job(job2);
+    // system.add_job(job1);
+    // system.add_job(job2);
 
-    std::cout << "Before: \n";
-    system.print_memory();
+    // std::cout << "Before: \n";
+    // system.print_memory();
 
-    system.add_job(job3);
+    // system.add_job(job3);
 
-    std::cout << "After: \n";
-    system.print_memory();
+    // std::cout << "After: \n";
+    // system.print_memory();
 
     // system.remove_job(job1);
 
